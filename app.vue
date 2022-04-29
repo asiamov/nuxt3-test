3c30240fa6138deba2970e3bf3c96f5998c16554
Alexander Siliamov <asiliamov@tuev-nord.de>
2022-04-29 10:51:08 +0200

    add

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Index Seite</h1>
+    <div>das ist HTML!!</div>
+    {{ data }}
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const { data } = useFetch("/api/hello");
+
+</script>
